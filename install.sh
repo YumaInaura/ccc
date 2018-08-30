b333e1d1b6602072483e0906f2269019c0b71d41
Yumainaura <yuma.inaura@gmail.com>
2018-08-31 07:36:09 +0900

    refactor install

--- install.sh
+++ install.sh
@@ -8,4 +8,12 @@ mkdir -p "$home_dir"
 rm -rf "$home_dir/lib"
 cp -rf ./lib $home_dir
 
+echo ===========================================
+echo "Installed ( $lib_dir )"
+echo ===========================================
+ls -la "$lib_dir"
 
+echo ===========================================
+echo Add build source line in your profile file
+echo ===========================================
+cat ./build_source
