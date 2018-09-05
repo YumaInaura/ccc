8ce84d233e3a749234721518dac4ed16381f3b67
Yumainaura <yuma.inaura@gmail.com>
2018-09-06 08:59:11 +0900

    fix install

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash -eu
 
-rm -rv "$HOME/eco"
-mkdir -p "$HOME/eco"
+readonly eco_install_path=${ECO_INSTALL_PATH:-"$HOME/eco"}
 
-cp -rfv ../eco/bin "$HOME/eco"
+if [[ ! -z "$eco_install_path" ]] && [[ ! "$eco_install_path" =~ /\//  ]]; then
+  rm -rv "$eco_install_path/"
+fi
+
+mkdir -p "$eco_install_path/eco"
+
+readonly current_directory=$(echo "$0" | sed -e "s/\/$(basename "$0")$//g")
+
+cp -rfv "$current_directory"/bin "$eco_install_path/"
+cp -rfv "$current_directory"/fbin "$eco_install_path/"
 
 echo ===========================================
 echo "Installed"
