1a277155bcadefb89455d121f1f7358e45e402fc
Yumainaura <yuma.inaura@gmail.com>
2018-09-16 11:03:24 +0900

    refact install

--- install.sh
+++ install.sh
@@ -15,10 +15,15 @@ readonly current_directory=$(echo "$0" | sed -e "s/\/$(basename "$0")$//g")
 cp -rfv "$current_directory"/bin "$eco_install_path/"
 cp -rfv "$current_directory"/fbin "$eco_install_path/"
 
-echo ===========================================
-echo "Installed"
-echo ===========================================
-echo Add PATH in your profile file
-echo ===========================================
-echo 'export PATH=$HOME/eco/bin:$PATH'
+cat <<"MESSAGE"
+===========================================
+Installed
+===========================================
+Add PATH in your profile file
+===========================================
+export PATH=$HOME/eco/bin:$PATH
+MESSAGE
+
+
+
 
